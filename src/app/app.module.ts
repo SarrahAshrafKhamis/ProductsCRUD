7c8a92626a9da42736491c0ea0ea88994fb59b7d
Sarrah Ashraf Khamis <sarrahashrafkhamis@gmail.com>
2022-02-27 15:25:23 +0200

    edit folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductDetailsComponent } from './product/product-details/product-details.component';
-import { ProductAddComponent } from './product/product-add/product-add.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product/product-details/product-details.component';
+import { ProductAddComponent } from './components/product/product-add/product-add.component';
 import { FormsModule } from '@angular/forms';
-import { ProductUpdateComponent } from './product/product-update/product-update.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { PowerPipe } from './pipes/power.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
 import { ReplacePipe } from './pipes/replace.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
-import { RatingComponent } from './rating/rating.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
